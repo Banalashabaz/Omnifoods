1dbcd0817f6df5e3555a6ea84e74523fc58dc0bb
shabaz <shabazalibanala205205@gmail.com>
2024-10-26 20:54:45 +0530

    Omnifoods

---
+++ new.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const textElement = document.getElementsByClassName("animatedText");
+    textElement.style.opacity = "1";
+    textElement.style.transform = "translateY(0)";
+});
